d2d1fb4403802c3759283b1e8fca2ef76ba0f8b6
Kshitiz-2002 <kshitizpandey2002@gmail.com>
2024-09-06 18:43:53 +0530

    chore: setup middleware for domain

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,39 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next'
 import { DM_Sans } from 'next/font/google'
-import "./globals.css";
-
+import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
 import { dark } from '@clerk/themes'
-import { ThemeProvider } from "@/providers/theme-provider";
+import { ThemeProvider } from '@/providers/theme-provider'
+import { Toaster } from '@/components/ui/toaster'
+import { Toaster as SonnarToaster } from '@/components/ui/sonner'
 
 const font = DM_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Zura",
-  description: "All in one Agency Solution",
-};
+  title: 'Zura',
+  description: 'All in one Agency Solution',
+}
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <ClerkProvider appearance={{ baseTheme: dark }}>
-      <html lang="en" suppressHydrationWarning>
-        <body className={font.className}>
-          <ThemeProvider attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange>
+    <html
+      lang="en"
+      suppressHydrationWarning
+    >
+      <body className={font.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
             {children}
-          </ThemeProvider>
-        </body>
-      </html>
-    </ClerkProvider>
-  );
+        </ThemeProvider>
+      </body>
+    </html>
+  )
 }
